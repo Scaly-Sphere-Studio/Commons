6bd998956bfb0b5bdce700f122bad2fe7511ec28
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2021-07-15 20:24:35 +0200

    Added cleanWeakPtrVector

--- inc/SSS/Commons/pointers.hpp
+++ inc/SSS/Commons/pointers.hpp
@@ -10,4 +10,15 @@ struct C_Ptr : public std::unique_ptr<_Ty, _Dx> {
     C_Ptr(_Ty* ptr) noexcept : std::unique_ptr<_Ty, _Dx>(ptr, func) {};
 };
 
+template <class T>
+void cleanWeakPtrVector(std::vector<std::weak_ptr<T>>& vector) {
+    vector.erase(
+        std::remove_if(
+            vector.begin(),
+            vector.end(),
+            [](std::weak_ptr<T> weak) { return weak.expired(); }),
+        vector.end()
+    );
+};
+
 __SSS_END
