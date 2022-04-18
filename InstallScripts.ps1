d2c893eb7a7dba22dd47a91f44aaced1d3551d71
Mériadec Lantonnet <meriadeclantonnet@gmail.com>
2022-04-19 01:46:35 +0200

    Delete InstallScripts.ps1

--- InstallScripts.ps1
+++
@@ -1,3 +0,0 @@
-git -C $PSScriptRoot submodule add https://github.com/Scaly-Sphere-Studio vcpkg_scripts;
-git -C $PSScriptRoot submodule foreach git fetch;
-git -C $PSScriptRoot submodule update --recursive --remote;
